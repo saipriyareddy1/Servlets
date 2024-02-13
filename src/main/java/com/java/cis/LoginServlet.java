93c0024c795fbbf90a04f562380cbff72a994d98
saipriyareddy1 <saipriyareddy.1119@gmail.com>
2024-02-13 16:08:08 -0600

    servlest created

---
+++ src/main/java/com/java/cis/LoginServlet.java
@@ -0,0 +1,26 @@
+package com.java.cis;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class LoginServlet extends HttpServlet {
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String validUsername = "saipriya";
+        String validPassword = "saipriya15";
+        String enteredUsername = request.getParameter("username");
+        String enteredPassword = request.getParameter("password");
+        boolean isValidUser = validUsername.equals(enteredUsername) && validPassword.equals(enteredPassword);
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<html><body>");
+        if (isValidUser) {
+            out.println("<h2>Login Successful</h2>");
+        } else {
+            out.println("<h2>Login Failed. Invalid username or password.</h2>");
+        }
+        out.println("</body></html>");
+    }
+}
